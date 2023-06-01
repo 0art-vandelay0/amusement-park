6b9624d94d955c28304ceb561486a63a30f531b3
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-01 12:35:54 -0700

    Add styles and content

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,21 @@ window.onload = function () {
         hideResults();
         const age = parseInt(document.querySelector("input#age").value);
         const height = parseInt(document.querySelector("input#height").value);
+
+        if (age >= 12 && height >= 60) {
+            document.getElementById("swings").removeAttribute("class");
+            document.getElementById("coaster").removeAttribute("class");
+            document.getElementById("tower").removeAttribute("class");
+            // Next, we're evaluating the Roller Coaster ride.
+        } else if (age >= 12 || height >= 48) {
+            document.getElementById("swings").removeAttribute("class");
+            document.getElementById("coaster").removeAttribute("class");
+        } else if (age >= 6) {
+            document.getElementById("swings").removeAttribute("class");
+            // Finally, we have our "catch-all" for when a user is less than 6 years old.
+        } else {
+            document.getElementById("sorry").removeAttribute("class");
+        }
+
     };
 };
